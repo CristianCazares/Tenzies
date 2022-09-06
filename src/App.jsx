b87de5268526c15e734aeef5b6c9756ccc15bfab
CristianCazares <cm_cjavier@hotmail.com>
2022-09-05 23:17:27 -0500

    feat: added interactions with dies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Die from './components/Die'
 import './App.css'
 
 function App() {
-  const [nums, setNums] = useState(generateNumbers())
-  const dieElements = nums.map(num => <Die number={num} />)
+  const [dice, setDice] = useState(generateDice([]))
+  /*
+  useEffect(() => {
+    dice.forEach(die => {
+      console.log(die)
+      if(die.isHeld === false)
+        return
+    });
+    console.log("you win")
+  }, [dice])
+  */
 
-  function generateNumbers() {
-    return Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6))
+  function generateRandomNumber() {
+    return Math.ceil(Math.random() * 6)
+  }
+
+  function generateDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      const die = {
+        id: `die${i}`,
+        value: generateRandomNumber(),
+        isHeld: false
+      }
+      newDice.push(die)
+    }
+    return newDice
+  }
+
+  const dieElements = dice.map(die =>
+    <Die
+      key={die.id}
+      number={die.value}
+      isHeld={die.isHeld}
+      handleClick={() => holdDie(die.id)}
+    />
+  )
+
+  function holdDie(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
+    })
+    )
   }
 
   const reroll = () => {
-    setNums(generateNumbers())
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld === true ?
+        die :
+        {...die, value : generateRandomNumber()}
+
+    }))
   }
+
 
 
 
--- src/components/Die/Die.jsx
+++ src/components/Die/Die.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./Die.css"
 
-const Die = ({ number }) => {
+const Die = (props) => {
   return (
-    <div className='die'>
-      <h1 className='die-number'>
-        {number}
+    <div
+      className={`${props.isHeld === true ? "die die-held" : "die"}`}
+      onClick={props.handleClick}
+    >
+      <h1 className="die-number">
+        {props.number}
       </h1>
     </div>
   )
